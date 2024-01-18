a17df50bdd2acb1cc1e83a1912d452199c0d835a
Ullas <ullasgowda42@gmail.com>
2024-01-18 23:06:52 +0530

    feat: add themes

---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#265073",
+    },
+    secondary: {
+      main: "#2D9596",
+    },
+  },
+});
+
+export default theme;
